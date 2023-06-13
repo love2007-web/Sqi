f4e35804db330863c8d92b999c52c3f77a6a53bf
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-06-13 11:44:08 +0100

    Second compoment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import FirstComponent from './FirstComponent';
 import Navbar from './Navbar';
+import SecondComponent from './SecondComponent';
 
 function App() {
   return (
     <>
       <Navbar/>
       <FirstComponent/>
+      <SecondComponent/>
     </>
   );
 }
