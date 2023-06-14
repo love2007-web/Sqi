d92dd223445a6f732136b5c7942b71640d119b93
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-06-14 08:49:25 +0100

    Done fourth and third component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import FirstComponent from './FirstComponent';
 import Navbar from './Navbar';
 import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
+import FourthComponent from './FourthComponent';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Navbar/>
       <FirstComponent/>
       <SecondComponent/>
+      <ThirdComponent/>
+      <FourthComponent/>
     </>
   );
 }
